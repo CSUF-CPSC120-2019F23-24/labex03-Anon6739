201d92187cc8ec9c468c0e0210f0dd6ece0bc53c
solisandrew117 <solisandrew117@cs104u-2d.acad.fullerton.edu>
2019-09-07 14:40:17 -0700

    Height Calculator

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,21 @@
 // This program calculates a person's height in feet (') and inches (").
+#include <iostream>
+#include <string>
+
+int main()
+{
+  int height_in_inches, feet, inches;
+
+  //User input of height_in_inches
+  std::cout << "Please enter the person's height in inches: ";
+  std::cin >> height_in_inches;
+
+  //Multiplying and Diving
+  feet = height_in_inches/12;
+  inches = height_in_inches%12;
+
+  //Person's height in feet
+  std::cout << "That person is " << feet <<"\'" << inches <<"\"";
+
+  return 0;
+}
